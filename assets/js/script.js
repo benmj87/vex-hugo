f90d75fa09c9c0f665f3dc78a98bc0d3fc9beaa6
Ben Johnston <ben.johnston@avcosystems.com>
2021-01-12 21:21:21 +0000

    Testing of modal feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,48 @@ $(window).on('load', function () {
 (function ($) {
   'use strict';
 
+  if (document.querySelector("#openModal")) {
+    var none = document.querySelector("#none");
+    var happyBirthday = document.querySelector("#happyBirthday");
+    var snipcartButton = document.querySelector("#snipcartButton");
+
+    none.addEventListener('change', () => {
+      if (none.checked) {
+        snipcartButton.setAttribute('data-item-custom2-value', 'None');
+      }
+    });
+    happyBirthday.addEventListener('change', () => {
+      if (happyBirthday.checked) {
+        snipcartButton.setAttribute('data-item-custom2-value', 'Happy Birthday');
+      }
+    });
+
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("openModal");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal
+    btn.onclick = function() {
+      modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+  }
   
   // product Slider
   $('.product-image-slider').slick({
