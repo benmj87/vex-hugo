05714a5514dd87aca2526961193a7c3f160a67c9
Ben Johnston <ben.johnston@avcosystems.com>
2021-01-13 21:35:37 +0000

    Tidy up modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,46 +7,24 @@ $(window).on('load', function () {
   'use strict';
 
   if (document.querySelector("#openModal")) {
-    var none = document.querySelector("#none");
-    var happyBirthday = document.querySelector("#happyBirthday");
     var snipcartButton = document.querySelector("#snipcartButton");
 
-    none.addEventListener('change', () => {
-      if (none.checked) {
-        snipcartButton.setAttribute('data-item-custom2-value', 'None');
-      }
+    $(".card").on("click", function(e) {
+      $(e.currentTarget).find("input[type=radio]").prop("checked", true);
+      snipcartButton.setAttribute("data-item-custom2-value", $(e.currentTarget).find("input[type=radio]").prop("value"));
     });
-    happyBirthday.addEventListener('change', () => {
-      if (happyBirthday.checked) {
-        snipcartButton.setAttribute('data-item-custom2-value', 'Happy Birthday');
-      }
-    });
-
-    // Get the modal
-    var modal = document.getElementById("myModal");
 
     // Get the button that opens the modal
     var btn = document.getElementById("openModal");
 
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
+    snipcartButton.addEventListener('click', () => {
+      $("#modalSelector").modal('hide');
+    });
 
     // When the user clicks on the button, open the modal
     btn.onclick = function() {
-      modal.style.display = "block";
-    }
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-      modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
-      }
-    }
+      $("#modalSelector").modal({});
+    }  
   }
   
   // product Slider
